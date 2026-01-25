b7126e8ed60de844cefc70ac1de34980e48a357b
Jay Lao <cjaylao447@gmail.com>
2026-01-25 18:03:23 +0800

    modified: layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -14,8 +14,6 @@ function Layout() {
   return (
     <div className={`min-h-screen ${isDarkMode ? 'dark bg-dark-bg text-body-text' : 'bg-[#285B9D] text-light-body-text'} transition-colors duration-300 font-sans leading-relaxed`}>
       <Head isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-    
-
       <AboutMe isDarkMode={isDarkMode} />
     </div>
   )
