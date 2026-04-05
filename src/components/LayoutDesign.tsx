a8a146b441600f7c0c63b712096308074cdc94f0
Jay Lao <cjaylao447@gmail.com>
2026-04-05 18:50:39 +0800

    🗑️ remove: eliminate unused background effects in LayoutDesign component

--- src/components/LayoutDesign.tsx
+++ src/components/LayoutDesign.tsx
@@ -27,23 +27,6 @@ export function LayoutDesign({ isDarkMode, children }: LayoutDesignProps) {
           size={500}
         />
 
-        <div className="fixed inset-0 pointer-events-none opacity-[0.02] z-[1]">
-          <div
-            className="absolute inset-0"
-            style={{
-              backgroundImage: `linear-gradient(${isDarkMode ? '#5EEEFF' : '#1DD0A7'} 1px, transparent 1px), linear-gradient(90deg, ${isDarkMode ? '#5EEEFF' : '#1DD0A7'} 1px, transparent 1px)`,
-              backgroundSize: '50px 50px'
-            }}
-          ></div>
-        </div>
-
-        <div
-          className="fixed inset-0 pointer-events-none opacity-[0.015] z-[2]"
-          style={{
-            backgroundImage: 'url("data:image/svg+xml,%3Csvg viewBox=\'0 0 400 400\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cfilter id=\'noiseFilter\'%3E%3CfeTurbulence type=\'fractalNoise\' baseFrequency=\'0.9\' numOctaves=\'4\' stitchTiles=\'stitch\'/%3E%3C/filter%3E%3Crect width=\'100%25\' height=\'100%25\' filter=\'url(%23noiseFilter)\'/%3E%3C/svg%3E")',
-          }}
-        ></div>
-
         <div className="fixed inset-0 pointer-events-none overflow-hidden z-[3]">
           <div
             className={`absolute top-0 right-1/4 w-[500px] h-[500px] rounded-full blur-[120px] ${isDarkMode ? 'bg-highlight/10' : 'bg-[#1DD0A7]/10'
