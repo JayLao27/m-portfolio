21d5f2c05f196952f87d552bbb736e057f3da04e
Jay Lao <cjaylao447@gmail.com>
2026-05-14 11:40:39 +0800

    🗑️ remove: eliminate loading screen logic from Layout component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,19 +4,13 @@ import { AboutMe } from './components/section/AboutMe/AboutMe'
 import { Hero } from './components/section/Hero/Hero'
 import { Projects } from './components/section/Projects/Projects'
 import { Contact } from './components/section/Contact/Contact'
-import LoadingScreen from './components/LoadingScreen'
 import { LayoutDesign } from './components/LayoutDesign'
 
 function Layout() {
   const [isDarkMode, setIsDarkMode] = useState(true)
-  const [isLoading, setIsLoading] = useState(true)
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode)
-  }
-
-  if (isLoading) {
-    return <LoadingScreen onLoadingComplete={() => setIsLoading(false)} duration={4000} />
   }
 
   return (
