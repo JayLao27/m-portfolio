7ba9783966197297e204475c080eae5d9991a2e7
Jay Lao <cjaylao447@gmail.com>
2026-05-28 23:42:12 +0800

    📦 new: add Chatbot component and theme toggle functionality

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -5,9 +5,11 @@ import { Hero } from './components/section/Hero/Hero'
 import { Projects } from './components/section/Projects/Projects'
 import { Contact } from './components/section/Contact/Contact'
 import { LayoutDesign } from './components/LayoutDesign'
+import { Chatbot } from './components/Chatbot'
 
 function Layout() {
   const [isDarkMode, setIsDarkMode] = useState(true)
+  const [isChatOpen, setIsChatOpen] = useState(false)
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode)
@@ -15,12 +17,23 @@ function Layout() {
 
   return (
     <LayoutDesign isDarkMode={isDarkMode}>
-      <Hero isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+      <Hero 
+        isDarkMode={isDarkMode} 
+        toggleTheme={toggleTheme} 
+        onChatOpen={() => setIsChatOpen(true)} 
+      />
       <AboutMe isDarkMode={isDarkMode} />
       <Projects isDarkMode={isDarkMode} />
       <Contact isDarkMode={isDarkMode} />
+      <Chatbot 
+        isDarkMode={isDarkMode} 
+        isOpen={isChatOpen} 
+        onClose={() => setIsChatOpen(false)} 
+        onOpen={() => setIsChatOpen(true)} 
+      />
     </LayoutDesign>
   )
 }
+
 
 export default Layout
